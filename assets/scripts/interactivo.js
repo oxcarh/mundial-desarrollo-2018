b8b28182b8aa427e6b45f28a338659751b47c8da
Oscar Hernandez <oxcarh@gmail.com>
2018-07-02 16:31:34 -0400

    Titulo y descripción para los videos

--- assets/scripts/interactivo.js
+++ assets/scripts/interactivo.js
@@ -526,6 +526,7 @@ var VisSimulator = (function () {
                 } else if(publicacion.tipo === "VID") {
                     publications_container.html(
                         publications_container.html() +
+                        '<p><strong>' + publicacion.titulo + '</strong><br/>' + publicacion.descripcion + '</p>' +
                         '<div class="video-container">' +
                         '<iframe width="1120" height="630" src="' + publicacion.url + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>' +
                         '</div>'
